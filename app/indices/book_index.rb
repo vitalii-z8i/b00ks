4740ec1af0ac7b05275d286597480f6e8fcf8212
vtl_pol <vitalii.zdanovskyi@gmail.com>
2014-07-22 10:03:20 +0300

    solr highlight. code cleanup

--- app/indices/book_index.rb
+++
@@ -1,5 +0,0 @@
-ThinkingSphinx::Index.define :book, with: :active_record do
-  indexes name
-  indexes author
-  indexes description
-end
--- app/indices/chapter_index.rb
+++
@@ -1,3 +0,0 @@
-ThinkingSphinx::Index.define :chapter, with: :active_record do
-  indexes inner_text, sortable: true
-end
